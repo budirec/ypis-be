41247521a3dfeea8cd346b4b3c1d751553f09ef0
Varun Kamra <varun.kamra22@gmail.com>
2023-04-20 13:09:07 +0530

    SALE-4 Added migrations.

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -2,3 +2,5 @@ create user ypuser with encrypted password 'secret@Pass';
 CREATE DATABASE yp;
 grant all privileges on database yp to ypuser;
 ALTER DATABASE yp OWNER to ypuser;
+\connect yp;
+CREATE EXTENSION "uuid-ossp";
